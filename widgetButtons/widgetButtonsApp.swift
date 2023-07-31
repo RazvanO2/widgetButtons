8aed4cc54eb3536e049817d32867d6fb18dbebb6
Răzvan Olariu <razvan.olariu136@gmail.com>
2023-07-31 09:41:26 +0300

    Initial Commit

---
+++ widgetButtons/widgetButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetButtonsApp.swift
+//  widgetButtons
+//
+//  Created by Razvan Olariu on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct widgetButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
